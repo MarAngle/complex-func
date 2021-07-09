fcbf2ef837519508479ba116855d0bf05b766bba
MarAngle <yinweiyingzhe@126.com>
2021-07-09 09:44:45 +0800

    Require注释

--- src/build/Require.js
+++ src/build/Require.js
@@ -4,13 +4,8 @@ import { getEnv } from './../data/environment/index'
 import noticeData from './../option/noticeData'
 import jsonToForm from './../data/object/jsonToForm'
 import printMsgAct from './../data/utils/printMsgAct'
+import getType from '../data/type/getType'
 
-/*
-option:
-headers
-responseType
-timeout
-*/
 class Require {
   constructor ({ api, option, rule, status }) {
     this.api = {
@@ -22,19 +17,26 @@ class Require {
       404: '很抱歉，资源未找到!',
       504: '网络超时!'
     }
-    this.list = []
     this.initApi(api)
     this.initService(option)
     this.initRule(rule)
     this.initStatus(status)
   }
-  // 加载api
+  /**
+   * 加载api
+   * @param {object} api
+   * @param {string} api.baseURL
+   */
   initApi (api) {
-    if (api.baseURL) {
+    if (api && api.baseURL) {
       this.api.baseURL = api.baseURL
     }
   }
-  // 创建option
+  /**
+   * 创建option
+   * @param {object} option
+   * @returns {object}
+   */
   buildOption (option = {}) {
     if (!option.headers) {
       option.headers = {}
@@ -44,15 +46,26 @@ class Require {
     }
     return option
   }
-  // 构建service
+  /**
+   * 构建service
+   * @param {*} option
+   * @returns {axios}
+   */
   buildService(option) {
     return axios.create(this.buildOption(option))
   }
-  // 创建service
+  /**
+   * 创建service
+   * @param {*} option
+   */
   initService (option) {
     this.service = this.buildService(option)
   }
-  // 加载规则
+  /**
+   * 加载规则
+   * @param {object} rule
+   * @param {RequireRule initdata} rule[prop]
+   */
   initRule (rule) {
     let firstProp
     for (let n in rule) {
@@ -69,13 +82,20 @@ class Require {
       this.printMsg(`默认的请求规则处理程序为[${this.rule.default._selfName()}]`, 'log')
     }
   }
-  // 加载状态翻译值
+  /**
+   * 加载状态翻译值
+   * @param {object} status
+   */
   initStatus (status = {}) {
     for (let n in status) {
       this.status[n] = status[n]
     }
   }
-  // 检查获取当前url对应的rule
+  /**
+   * 检查获取当前url对应的rule
+   * @param {string} url
+   * @returns {RequireRule}
+   */
   checkRule (url) {
     for (let n in this.rule) {
       let fg = this.rule[n].checkUrl(url)
@@ -85,7 +105,11 @@ class Require {
     }
     return this.rule.default
   }
-  // 格式化URL
+  /**
+   * 格式化URL
+   * @param {string} url
+   * @returns {string}
+   */
   _formatUrl (url) {
     if (this.formatUrl) {
       return this.formatUrl(url, this.api.baseURL)
@@ -97,7 +121,11 @@ class Require {
       return url
     }
   }
-  // 调用service进行axios请求
+  /**
+   * 调用service进行axios请求
+   * @param {*} optionData
+   * @returns {Promise}
+   */
   ajax (optionData = {}) {
     return new Promise((resolve, reject) => {
       this.service(optionData).then(response => {
@@ -107,89 +135,107 @@ class Require {
       })
     })
   }
-
-  // 传参检查和格式化
-  // url,
-  // data = {},
-  // params = {},
-  // headers = {},
-  // token,
-  // requestDataType = 'json',
-  // requestCurrentDataType = 'json'
-  // responseFormat = true
-  // responseType = 'json'
-  // responseFormat = true
-  check (optionData) {
+  /**
+   * 传参检查和格式化
+   * @param {object} optionData 参数
+   * @param {string} optionData.url 请求地址
+   * @param {string} optionData.method 请求方式,默认为get
+   * @param {object} optionData.params url(query参数)
+   * @param {object} optionData.data body参数
+   * @param {string} optionData.headers header参数
+   * @param {string | object | any[]} optionData.token token
+   * @param {'arraybuffer', 'blob', 'document', 'json', 'text', 'stream'} optionData.responseType 返回数据类型
+   * @param {'json' | 'formdata'} optionData.requestDataType 接口需要的数据类型
+   * @param {'json' | 'formdata'} optionData.requestCurrentDataType 当前data的数据类型
+   * @param {boolean} optionData.responseFormat 是否对返回数据进行分析和格式化,默认为true
+   * @param {object} [defaultOptionData] 默认参数重置method/requestDataType/requestCurrentDataType/responseType
+   * @returns {check}
+   */
+  check (optionData, defaultOptionData = {}) {
     let check = {
       next: true,
       code: '',
       msg: ''
     }
     // 检查参数
-    if (!optionData) {
+    if (getType(optionData, true) != 'object') {
       check.next = false
       check.code = 'undefined optionData'
       check.msg = '未定义请求数据'
-    }
-    // 检查URL
-    if (check.next) {
+    } else {
       optionData.url = this._formatUrl(optionData.url)
+      // 检查URL
       if (!optionData.url) {
         check.next = false
         check.code = 'undefined URL'
         check.msg = '未定义请求地址'
-      }
-    }
-    // 检查RULE
-    let ruleItem
-    if (check.next) {
-      ruleItem = this.checkRule(optionData.url)
-      if (!ruleItem) {
-        check.next = false
-        check.code = 'undefined rule'
-        check.msg = '未检索到对应规则'
-      }
-    }
-    // 添加默认值
-    if (!optionData.data) {
-      optionData.data = {}
-    }
-    if (!optionData.params) {
-      optionData.params = {}
-    }
-    if (!optionData.headers) {
-      optionData.headers = {}
-    }
-    if (!optionData.requestDataType) {
-      optionData.requestDataType = 'json'
-    }
-    if (!optionData.requestCurrentDataType) {
-      optionData.requestCurrentDataType = 'json'
-    }
-    if (!optionData.responseType) {
-      optionData.responseType = 'json'
-    }
-    if (optionData.responseFormat === undefined) {
-      optionData.responseFormat = true
-    }
-    // RULE NEXT
-    if (check.next) {
-      let ruleCheck = ruleItem.format(optionData)
-      if (ruleCheck && !ruleCheck.next) {
-        check.next = false
-        check.code = ruleCheck.code
-        check.msg = ruleCheck.msg
-        ruleItem.tokenFail(ruleCheck.prop)
       } else {
-        check.ruleItem = ruleItem
+        // 检查RULE
+        let ruleItem = this.checkRule(optionData.url)
+        if (!ruleItem) {
+          check.next = false
+          check.code = 'undefined rule'
+          check.msg = '未检索到对应规则'
+        } else {
+          // 添加默认值
+          if (!optionData.method) {
+            optionData.method = defaultOptionData.method || 'get'
+          }
+          if (!optionData.data) {
+            optionData.data = {}
+          }
+          if (!optionData.params) {
+            optionData.params = {}
+          }
+          if (!optionData.headers) {
+            optionData.headers = {}
+          }
+          if (!optionData.responseType) {
+            optionData.responseType = defaultOptionData.responseType || 'json'
+          }
+          if (!optionData.requestDataType) {
+            optionData.requestDataType = defaultOptionData.requestDataType || 'json'
+          }
+          if (!optionData.requestCurrentDataType) {
+            optionData.requestCurrentDataType = defaultOptionData.requestCurrentDataType || 'json'
+          }
+          if (optionData.responseFormat === undefined) {
+            optionData.responseFormat = defaultOptionData.responseFormat === undefined ? true : defaultOptionData.responseFormat
+          }
+          // RequireRule检查
+          let ruleCheck = ruleItem.format(optionData)
+          if (ruleCheck && !ruleCheck.next) {
+            check.next = false
+            check.code = ruleCheck.code
+            check.msg = ruleCheck.msg
+            ruleItem.tokenFail(ruleCheck.prop)
+          } else {
+            check.ruleItem = ruleItem
+          }
+        }
       }
     }
     return check
   }
-  // 请求，自动化处理
-  require (optionData) {
+  /**
+   * 请求主函数
+   * @param {object} optionData 参数
+   * @param {string} optionData.url 请求地址
+   * @param {string} optionData.method 请求方式,默认为get
+   * @param {object} optionData.params url(query参数)
+   * @param {object} optionData.data body参数
+   * @param {string} optionData.headers header参数
+   * @param {string | object | any[]} optionData.token token
+   * @param {'arraybuffer', 'blob', 'document', 'json', 'text', 'stream'} optionData.responseType 返回数据类型
+   * @param {'json' | 'formdata'} optionData.requestDataType 接口需要的数据类型
+   * @param {'json' | 'formdata'} optionData.requestCurrentDataType 当前data的数据类型
+   * @param {boolean} optionData.responseFormat 是否对返回数据进行分析和格式化,默认为true
+   * @param {object} [defaultOptionData] 默认参数重置method/requestDataType/requestCurrentDataType/responseType
+   * @returns {optionData}
+   */
+  require (optionData, defaultOptionData) {
     return new Promise((resolve, reject) => {
-      let check = this.check(optionData)
+      let check = this.check(optionData, defaultOptionData)
       if (check.next) {
         if (optionData.requestDataType == 'formdata') {
           optionData.headers['Content-Type'] = 'multipart/form-data'
@@ -210,7 +256,9 @@ class Require {
       }
     })
   }
-  // 请求下一步操作
+  /**
+   * 请求下一步操作
+   */
   requireNext (optionData, check) {
     return new Promise((resolve, reject) => {
       this.ajax(optionData).then(response => {
@@ -246,7 +294,13 @@ class Require {
       })
     })
   }
-  // 请求失败回调
+  /**
+   * 请求失败回调
+   * @param {*} error
+   * @param {*} optionData
+   * @param {*} ruleItem
+   * @returns
+   */
   requireFail (error, optionData, ruleItem) {
     let errRes = {
       status: 'fail',
@@ -271,7 +325,11 @@ class Require {
     }
     return errRes
   }
-  // 获取status翻译值
+  /**
+   * 获取status翻译值
+   * @param {number} status
+   * @returns {string}
+   */
   parseStatus (status) {
     if (status && this.status[status]) {
       return this.status[status]
@@ -279,7 +337,13 @@ class Require {
       return ''
     }
   }
-  // 自动显示失败msg
+  /**
+   * 自动显示失败msg
+   * @param {*} msgOption
+   * @param {*} content
+   * @param {*} type
+   * @param {*} title
+   */
   showFailMsg (msgOption, content, type, title) {
     if (msgOption === undefined || msgOption === true) {
       msgOption = {
@@ -308,30 +372,84 @@ class Require {
       }
     }
   }
-  // get请求
-  get (optionData = {}) {
-    optionData.method = 'get'
-    return this.require(optionData)
+  /**
+   * get请求
+   * @param {object} optionData 参数
+   * @param {string} optionData.url 请求地址
+   * @param {string} optionData.method 请求方式,默认为get
+   * @param {object} optionData.params url(query参数)
+   * @param {object} optionData.data body参数
+   * @param {string} optionData.headers header参数
+   * @param {string | object | any[]} optionData.token token
+   * @param {'arraybuffer', 'blob', 'document', 'json', 'text', 'stream'} optionData.responseType 返回数据类型
+   * @param {'json' | 'formdata'} optionData.requestDataType 接口需要的数据类型
+   * @param {'json' | 'formdata'} optionData.requestCurrentDataType 当前data的数据类型
+   * @param {boolean} optionData.responseFormat 是否对返回数据进行分析和格式化,默认为true
+   * @returns {Promise}
+   */
+  get (optionData) {
+    return this.require(optionData, { method: 'get' })
   }
-  // post请求
-  post (optionData = {}) {
-    optionData.method = 'post'
-    return this.require(optionData)
+  /**
+   * post请求
+   * @param {object} optionData 参数
+   * @param {string} optionData.url 请求地址
+   * @param {string} optionData.method 请求方式,默认为post
+   * @param {object} optionData.params url(query参数)
+   * @param {object} optionData.data body参数
+   * @param {string} optionData.headers header参数
+   * @param {string | object | any[]} optionData.token token
+   * @param {'arraybuffer', 'blob', 'document', 'json', 'text', 'stream'} optionData.responseType 返回数据类型
+   * @param {'json' | 'formdata'} optionData.requestDataType 接口需要的数据类型
+   * @param {'json' | 'formdata'} optionData.requestCurrentDataType 当前data的数据类型
+   * @param {boolean} optionData.responseFormat 是否对返回数据进行分析和格式化,默认为true
+   * @returns {Promise}
+   */
+  post (optionData) {
+    return this.require(optionData, { method: 'post' })
   }
-  // post请求=>json转换成formdata
-  postform (optionData = {}) {
-    optionData.method = 'post'
-    optionData.requestDataType = 'formdata'
-    return this.require(optionData)
+  /**
+   * post请求form类型,requestDataType默认为formdata
+   * @param {object} optionData 参数
+   * @param {string} optionData.url 请求地址
+   * @param {string} optionData.method 请求方式,默认为post
+   * @param {object} optionData.params url(query参数)
+   * @param {object} optionData.data body参数
+   * @param {string} optionData.headers header参数
+   * @param {string | object | any[]} optionData.token token
+   * @param {'arraybuffer', 'blob', 'document', 'json', 'text', 'stream'} optionData.responseType 返回数据类型
+   * @param {'json' | 'formdata'} optionData.requestDataType 接口需要的数据类型
+   * @param {'json' | 'formdata'} optionData.requestCurrentDataType 当前data的数据类型
+   * @param {boolean} optionData.responseFormat 是否对返回数据进行分析和格式化,默认为true
+   * @returns {Promise}
+   */
+  postform (optionData) {
+    return this.require(optionData, { method: 'post', requestDataType: 'formdata' })
   }
-  // post请求=>formdata参数
-  postfile (optionData = {}) {
-    optionData.method = 'post'
-    optionData.requestDataType = 'formdata'
-    optionData.requestCurrentDataType = 'formdata'
-    return this.require(optionData)
+    /**
+   * post请求formfile类型,requestDataType/requestCurrentDataType默认为formdata
+   * @param {object} optionData 参数
+   * @param {string} optionData.url 请求地址
+   * @param {string} optionData.method 请求方式,默认为post
+   * @param {object} optionData.params url(query参数)
+   * @param {object} optionData.data body参数
+   * @param {string} optionData.headers header参数
+   * @param {string | object | any[]} optionData.token token
+   * @param {'arraybuffer', 'blob', 'document', 'json', 'text', 'stream'} optionData.responseType 返回数据类型
+   * @param {'json' | 'formdata'} optionData.requestDataType 接口需要的数据类型
+   * @param {'json' | 'formdata'} optionData.requestCurrentDataType 当前data的数据类型
+   * @param {boolean} optionData.responseFormat 是否对返回数据进行分析和格式化,默认为true
+   * @returns {Promise}
+   */
+  postfile (optionData) {
+    return this.require(optionData, { method: 'post', requestDataType: 'formdata', requestCurrentDataType: 'formdata' })
   }
-  // token删除
+  /**
+   * 删除token
+   * @param {string} tokenName token名称
+   * @param {string} prop 对应的rule.prop
+   * @returns {boolean}
+   */
   removeToken (tokenName, prop = 'default') {
     if (this.rule[prop]) {
       return this.rule[prop].removeToken(tokenName)
@@ -340,7 +458,12 @@ class Require {
       return false
     }
   }
-  // token设置
+  /**
+   * 设置token
+   * @param {string} tokenName token名称
+   * @param {*} data token值
+   * @param {string} prop 对应的rule.prop
+   */
   setToken (tokenName, data, prop = 'default') {
     if (this.rule[prop]) {
       this.rule[prop].setToken(tokenName, data)
@@ -348,7 +471,11 @@ class Require {
       this.printMsg(`未找到[${tokenName}:${prop}]对应的规则处理程序！`)
     }
   }
-  // token获取
+  /**
+   * 获取指定token的值
+   * @param {string} tokenName token名称
+   * @param {string} prop 对应的rule.prop
+   */
   getToken (tokenName, prop = 'default') {
     if (this.rule[prop]) {
       return this.rule[prop].getToken(tokenName)
@@ -357,7 +484,6 @@ class Require {
       return false
     }
   }
-
   _selfName () {
     let ruleName = []
     for (let n in this.rule) {
