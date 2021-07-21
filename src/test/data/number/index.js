73a1a908bd42371588cc21dff40c5cf6806136bb
MarAngle <yinweiyingzhe@126.com>
2021-07-21 14:36:22 +0800

    测试用例更新

--- src/test/data/number/index.js
+++ src/test/data/number/index.js
@@ -3,22 +3,45 @@ import formatNum from './../../../data/number/formatNum'
 import getNum from './../../../data/number/getNum'
 import getRandomNum from './../../../data/number/getRandomNum'
 
-runText(function() {
-  // console.log(formatNum(' 111 '))
-  // console.log(formatNum(undefined))
-  // console.log(formatNum(null))
-  // console.log(formatNum(Symbol('1')))
-  // console.log(formatNum({
-  //   name: 1
-  // }))
-  // console.log(formatNum({
-  //   name: 1,
-  //   toString() {
-  //     return '111'
-  //   }
-  // }))
-  // const maxNumber = 9007199254740992
-  // console.log(maxNumber + maxNumber)
-  // console.log(9007199254740990.99)
-  // console.log(getNum(9007199254740991.9 - 100.9))
-}, 'number相关模块错误');
+runText(function({ checkSame, showError }) {
+  checkSame(formatNum('111'), 111, 'formatNum:字符串转换数字错误')
+  checkSame(formatNum(' 111'), 111, 'formatNum:字符串空格转换数字错误')
+  checkSame(formatNum(' 111 '), 111, 'formatNum:字符串空格转换数字错误')
+  checkSame(formatNum({}), Number.NaN, 'formatNum:对象将转换为NaN')
+  checkSame(formatNum({
+    toString() {
+      return '1111'
+    }
+  }), 1111, 'formatNum:对象实际会调用toString转换')
+  checkSame(formatNum(Symbol('1')), Number.NaN, 'formatNum:Symbol将转换为NaN')
+
+  checkSame(getNum(111.555), 111.56, 'getNum:默认四舍五入并保留2位')
+  checkSame(getNum({}), 0, 'getNum:默认NANZERO，NaN数据会格式化为0')
+  checkSame(getNum({}, 'round', 2, false), Number.NaN, 'getNum:NAN不自动格式化')
+  checkSame(getNum(111.555, 'floor', 1), 111.5, 'getNum:向下取整1位')
+  checkSame(getNum(111.444, 'ceil', 1), 111.5, 'getNum:向上取整1位')
+
+  const maxNumber = 9007199254740992
+
+  let checkRandom = false
+  if (checkRandom) {
+    const numMax = 5000
+    const max = 5
+    let cache = []
+    let per = numMax / max
+    for (let n = 0; n < numMax; n++) {
+      let num = getRandomNum(0, max)
+      if (cache[num]) {
+        cache[num]++
+      } else {
+        cache[num] = 1
+      }
+    }
+    for (let i = 0; i < max; i++) {
+      let rate = cache[i] / per
+      if (rate <= 0.8 || rate >= 1.2) {
+        showError('概率未按照需要产生，误差为:' + rate)
+      }
+    }
+  }
+}, 'number');
--- src/test/main.js
+++ src/test/main.js
@@ -1,14 +1,31 @@
 import isSame from '../data/type/isSame'
 
+/**
+ * 输出错误
+ * @param {String} error
+ * @throws
+ */
 function showError(error) {
   throw new Error(error)
 }
+/**
+ * 检查数据，不同时输出错误
+ * @param {*} value
+ * @param {*} other
+ * @param {string} error
+ */
 function checkSame(value, other, error) {
   if (!isSame(value, other)) {
+    console.error(value, other)
     showError(error)
   }
 }
 
+/**
+ * 运行测试
+ * @param {Function} fn
+ * @param {string} log
+ */
 function runText(fn, log) {
   try {
     fn({
