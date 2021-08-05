7a2ac9d821d802528a8999ce138aec4a2bb2a02e
MarAngle <yinweiyingzhe@126.com>
2021-08-05 13:33:26 +0800

    promiseAllFinished数量计算错误的问题

--- src/data/utils/promiseAllFinished.js
+++ src/data/utils/promiseAllFinished.js
@@ -1,42 +1,42 @@
 import isPromise from '../type/isPromise'
 
 /**
- * next
- * @param {number} remainder
- * @param {*} resolve
- * @param {*[]} resList
- * @param {number} index
- * @param {*} res
- */
-function next(remainder, resolve, resList, index, res) {
-  resList[index] = res
-  remainder--
-  if (remainder == 0) {
-    resolve(resList)
-  }
-}
-
-/**
  * Promise.allFinished
  * @param {Promise[]} list Promise列表
  * @returns {Promise}
  */
 function promiseAllFinished(list) {
   return new Promise((resolve) => {
-    let resList = []
+    /**
+     * next
+     * @param {number} remainder
+     * @param {*} resolve
+     * @param {*[]} resList
+     * @param {number} index
+     * @param {*} res
+     */
+    function next(index, res) {
+      resList[index] = res
+      remainder--
+      if (remainder == 0) {
+        resolve(resList)
+      }
+    }
+    var remainder
+    var resList = []
     if (list && list.length > 0) {
       let size = list.length
-      let remainder = size
+      remainder = size
       for (let n = 0; n < size; n++) {
         let item = list[n]
         if (isPromise(item)) {
           item.then(res => {
-            next(remainder, resolve, resList, n, res)
+            next(n, { status: 'success', data: res })
           }, err => {
-            next(remainder, resolve, resList, n, err)
+            next(n, { status: 'fail', data: err })
           })
         } else {
-          next(remainder, resolve, resList, n, undefined)
+          next(n, { status: 'fail', data: undefined })
         }
       }
     } else {
