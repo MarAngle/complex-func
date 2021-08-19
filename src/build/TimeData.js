dfd8f569eb432204f5f263ae4fedeeb5abe80b27
MarAngle <yinweiyingzhe@126.com>
2021-08-19 10:17:16 +0800

    时间暂停

--- src/build/TimeData.js
+++ src/build/TimeData.js
@@ -1,35 +1,37 @@
+const baseFormat = 'YYYY/MM/DD HH:mm:ss'
+const targetFormat = 'YYYY/MM/DD HH:mm:ss'
 
 let timeOption = {
   data: {
     year: {
       func: 'getFullYear',
-      name: 'Y',
+      name: 'YYYY',
       min: 2,
       offset: 0
     },
     month: {
       func: 'getMonth',
-      name: 'M',
+      name: 'MM',
       offset: 0
     },
-    day: {
+    date: {
       func: 'getDate',
-      name: 'D',
+      name: 'DD',
       offset: 0
     },
     hour: {
       func: 'getHours',
-      name: 'H',
+      name: 'HH',
       offset: 0
     },
     min: {
       func: 'getMinutes',
-      name: 'm',
+      name: 'mm',
       offset: 0
     },
     sec: {
       func: 'getSeconds',
-      name: 's',
+      name: 'ss',
       offset: 0
     }
   }
@@ -41,18 +43,45 @@ class TimeData {
       format: ''
     }
     this.time = {}
-    this.data = ''
+    this.data = {
+      year: '',
+      month: '',
+      date: '',
+      hour: '',
+      min: '',
+      sec: '',
+      week: ''
+    }
     this.build(data, format)
   }
   build(data, format) {
     if (!format) {
-      let time = new Date(data)
-      for (let n in timeOption.data) {
-        let timeItem = timeOption.data[n]
-        this.time[n] = time[timeItem.func]() + timeItem.offset
-      }
-    } else {
+      format = baseFormat
+    }
+    let date
+    if (format === 'X') {
+      date = new Date(data * 1000)
+    } else if (format === 'x') {
+      date = new Date(data)
+    }
+    date = this.buildByFormat(data, format)
+    // if (!format) {
+    //   let time = new Date(data)
+    //   for (let n in timeOption.data) {
+    //     let timeItem = timeOption.data[n]
+    //     this.time[n] = time[timeItem.func]() + timeItem.offset
+    //   }
+    // } else {
 
+    // }
+  }
+  buildByFormat(data, format) {
+    let currentDate = new Date()
+    for (let n in timeOption.data) {
+      let optionItem = timeOption.data[n]
+      let index = format.indexOf(optionItem.name)
+      if (index > -1) {
+      }
     }
   }
   format() {
