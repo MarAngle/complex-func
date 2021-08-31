ca09e726505e573a6013a8cf7e6c2232c9773b48
MarAngle <yinweiyingzhe@126.com>
2021-08-31 10:45:06 +0800

    优化updateOption注释

--- src/data/object/formatUpdateDataOption.js
+++ src/data/object/formatUpdateDataOption.js
@@ -4,12 +4,12 @@ import getLimitData from './../utils/getLimitData'
  * 格式化UpdateOption,updateDataWidthOption函数的设置项
  * @param {object} [option] 用户设置的设置项
  * @param {'total' | 'add'} [option.type] 全更新/附加更新判断值
- * @param {boolean} [option.reset] 全更新/附加更新判断值
+ * @param {boolean} option.reset 重置判断值，默认为真，类型不同且reset为真时，无法将以前的数据作为基准，将会对源数据的对应值根据类型重置后再进行深拷贝循环
+ * @param {boolean | number} [option.depth] 属性深度判断值
  * @param {LimitData} [option.limitData] 属性限制判断值
  * @param {object} [option.limit] 属性限制判断值limitData生成参数
  * @param {'forbid' | 'allow'} [option.limit.type] 属性限制判断值limitData生成参数-type
  * @param {string[]} [option.limit.list] 属性限制判断值limitData生成参数-list
- * @param {boolean | number} [option.depth] 属性深度判断值
  * @param {*} defaultOption 默认值
  * @returns {object}
  */
